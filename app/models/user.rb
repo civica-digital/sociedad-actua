f0882d7e5c1a25d84a0a2ea94a38a3d59499eb13
Miguel Angel Gordian <os.aioria@gmail.com>
2015-10-13 20:51:13 -0500

    Agrega soporte para roles a el modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,22 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  rolify :role_cname => 'Role'
+
+  has_and_belongs_to_many :users, :join_table => :users_users
+  belongs_to :resource, :polymorphic => true
+
+  validates :resource_type,
+            :inclusion => { :in => Rolify.resource_types },
+            :allow_nil => true
+
+  scopify
+
   def set_default_role
     if User.count == 0
-      self.role ||= :admin
+      self.add_role :admin
     else
-      self.role ||= :user
+      self.add_role :user
     end
   end
 
@@ -15,7 +26,7 @@ class User < ActiveRecord::Base
       user.provider = auth['provider']
       user.uid = auth['uid']
       if auth['info']
-         user.name = auth['info']['name'] || ""
+        user.name = auth['info']['name'] || ""
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,6 @@
-describe User do
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
 
   before(:each) { @user = FactoryGirl.create(:user) }
 
@@ -6,8 +8,19 @@ describe User do
 
   it { should respond_to(:name) }
 
-  it "#name returns a string" do
-    expect(@user.name).to match 'Test User'
+  it { should be_valid }
+
+  it { expect(@user.name).to be_instance_of String  }
+
+  it "is admin if the first is" do
+    is_expected.to have_role(:admin)
   end
+
+  it "is a user if isn't a admin" do
+    another_user = FactoryGirl.create(:user)
+
+    expect(another_user).to have_role(:user)
+  end
+
 
 end
