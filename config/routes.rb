a320408e4f37898135fab6692377707f9bc2d0fd
Miguel Angel Gordian <os.aioria@gmail.com>
2015-10-14 02:27:39 -0500

    Asigna nuevas rutas para login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root to: 'visitors#index'
+
   get '/auth/:provider/callback' => 'sessions#create'
-  get '/signin' => 'sessions#new', :as => :signin
-  get '/signout' => 'sessions#destroy', :as => :signout
+
+  as :user do
+    get  '/register' => 'devise/registrations#new', :as => :registration_new
+    post '/register' => 'devise/registrations#create', :as => :registration
+    get  '/login' => 'devise/sessions#new', :as => :signin
+    post '/login' => 'devise/sessions#create', :as => :session
+    get  '/logout' => 'devise/sessions#destroy', :as => :signout
+  end
+
   get '/auth/failure' => 'sessions#failure'
 end
