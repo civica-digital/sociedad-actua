321cc08ff546c3c8c5b4cfc26af0505ebb9290e0
Miguel Angel Gordian <os.aioria@gmail.com>
2015-10-13 20:38:32 -0500

    Agrega el modelo Role

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729191148) do
+ActiveRecord::Schema.define(version: 20151013230208) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  create_table "collaborators", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "role"
+    t.integer  "resource_id"
+    t.string   "resource_type"
   end
+
+  add_index "users", ["name", "resource_type", "resource_id"], name: "index_users_on_name_and_resource_type_and_resource_id"
+  add_index "users", ["name"], name: "index_users_on_name"
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+
+  create_table "users_users", id: false, force: :cascade do |t|
+    t.integer "user_id"
+  end
+
+  add_index "users_users", ["user_id", "user_id"], name: "index_users_users_on_user_id_and_user_id"
 
 end
