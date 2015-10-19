06f52a07c447db91751e2eee86014bc97eb2e21a
Miguel Angel Gordian <os.aioria@gmail.com>
2015-10-19 06:31:48 -0500

    Cambio de variable en `organization_specs`

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  before(:each) { @user = FactoryGirl.create(:organization) }
+  before(:each) { @organization = FactoryGirl.create(:organization) }
 
 
 
   describe "Should be valid and verify the required fields" do
-    subject { @user }
+    subject { @organization }
 
     it { should be_valid }
 
     it "should have always email" do
-      @user.email = nil
-      expect(@user).to_not be_valid
+      @organization.email = nil
+      expect(@organization).to_not be_valid
     end
 
     it "should have always a name"  do
-      @user.name = nil
-      expect(@user).to_not be_valid
+      @organization.name = nil
+      expect(@organization).to_not be_valid
     end
 
     it "should have always an organization's type"  do
-      @user.type_organization = nil
-      expect(@user).to_not be_valid
+      @organization.type_organization = nil
+      expect(@organization).to_not be_valid
     end
   end
 end
