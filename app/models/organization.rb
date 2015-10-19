e030c6c2fe4fb804c6702f5ed6d48a61acc1fdf7
Miguel Angel Gordian <os.aioria@gmail.com>
2015-10-19 01:21:59 -0500

    Agrega validaciones a los campos de organización

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,24 @@
 class Organization < ActiveRecord::Base
   has_one :user, as: :profile
+
+  validates :name, :type_organization, :email, :presence =>  true
+  validates :name, :email, :uniqueness => true
+  validates :name, :length => { :minimum => 2 }
+  validates :mision, :length => { :maximum => 500 }, allow_blank: true
+  validates :type_organization, :inclusion => { :in => %w{ACs ABPs Colectivos Grupos },
+                                                :message => "%{value} no es un tipo de organización valido"}
+
+  # TODO: validar formato de correo
+  # validates :email,
+
+  # TODO: validar formato de
+  # validates :rfc
+
+  validates :zip, :format => { :with => /[0-9]{5}/}
+
+  # TODO: validar numero de telefono
+  # validates: telephone
+
+  # TODO: validar el formato de url para sitio y redes sociales
+  # validates_with URLValidator, fields: [:site, :blog, facebook, youtube, instagram, twitter]
 end
