2416128cd741207a89a074fc43c4bc23d235a040
Miguel Angel Gordian <os.aioria@gmail.com>
2015-10-19 03:57:32 -0500

    Refactor en la validación del tipo de la organización

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,27 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_sign_up_params, only: [:create]
-
-
-  # POST /register
-  def create
-    unless ['collaborator', 'organization', 'investor'].include? params["user"]["role"]
-      flash[:notice] = "Este elemento no pertence a algun perfil valido"
-      redirect_to registration_new_url
-      return
-    end
-
-    super
-    # TODO: verificar por que el rol no fue actualizado despues
-    # de agregar strong parameters
-    @user.add_role params["user"]["role"]
-  end
-
+  before_action :valid_organizations_type, only: [:create]
 
   protected
   def after_sign_up_path_for(resource)
     eval("new_#{params["user"]["role"].singularize}_path") # TODO: Cambiar a forma segura
   end
-
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
@@ -30,6 +14,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
       devise_parameter_sanitizer.for(:sign_up) {
           |u| u.permit(*registration_params)
       }
+    end
+  end
+
+  def valid_organizations_type
+    unless ['collaborator', 'organization', 'investor'].include? params["user"]["role"]
+      flash[:notice] = "Este elemento no pertence a algun perfil valido"
+      redirect_to registration_new_url
+      return
     end
   end
 
