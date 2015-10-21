463ccb2a5ae7f2ca332006eeb45d764ef2b500fd
Miguel Angel Gordian <os.aioria@gmail.com>
2015-10-21 18:35:47 -0500

    Validación de parametros existentes en el controller `registration`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_devise_permitted_parameters, if: :devise_controller?
-
-  protected
-  def configure_devise_permitted_parameters
-    registration_params = [:email, :password, :password_confirmation, :roles]
-
-    if params[:action] == 'create'
-      devise_parameter_sanitizer.for(:sign_up) {
-          |u| u.permit(registration_params)
-      }
-    end
-  end
-
 end
