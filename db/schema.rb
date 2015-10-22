0a006b423a6a95976101b0757df2e59462405c74
Andres Lopez De Leon <agldeleon@gmail.com>
2015-10-22 18:11:47 -0500

    Add columns to investor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022195324) do
+ActiveRecord::Schema.define(version: 20151022230631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20151022195324) do
     t.string   "facebook_url"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "address"
+    t.string   "neighborhood"
+    t.string   "city"
+    t.string   "telephone"
+    t.string   "zipcode"
+    t.string   "email"
   end
 
   create_table "organizations", force: :cascade do |t|
