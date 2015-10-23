3fe80875f00c47d805795e5ecb2d1bd004d2b085
Miguel Angel Gordian <os.aioria@gmail.com>
2015-10-23 17:47:12 -0500

    Cambia fog por carrierwave-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,18 @@
 CarrierWave.configure do |config|
-
-  if Rails.env.production?
-    config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_KEY'],
-      :aws_secret_access_key  => ENV['AWS_SECRET'],
+  unless Rails.env.test?
+    config.aws_credentials = {
+      :access_key_id      => ENV['AWS_KEY'],
+      :secret_access_key  => ENV['AWS_SECRET'],
       :region                 => ENV['AWS_REGION']
     }
-    config.fog_directory  = ENV['S3_BUCKET']
-    config.fog_public = true
+    config.aws_bucket = ENV['S3_BUCKET']
+    config.aws_acl = 'public-read'
   end
 
 
 
   config.permissions = 0600
 
-  config.storage(Rails.env.test? || Rails.env.development? ? :file : :fog)
+  config.storage(Rails.env.test? ? :file : :aws)
   config.enable_processing = false if Rails.env.test?
 end
