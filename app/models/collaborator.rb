ee623f3077503c828abe0ff32b4fa9d9d862bd50
Carlos Castellanos <rockarlos@me.com>
2015-10-28 17:10:05 -0600

    valida longitud de nombre y descripcion de colaboradores en update

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -5,8 +5,8 @@ class Collaborator < ActiveRecord::Base
   validates :email, :type_collaborator, :description, presence:  true, on: :update
   validates :email, uniqueness: true
 
-  validates :name, :length => { :minimum => 2 }
-  validates :description, :length => { :maximum => 500 }, allow_blank: false
+  validates :name, :length => { :minimum => 2 }, on: :update
+  validates :description, :length => { :maximum => 500 }, allow_blank: false, on: :update
 
   validates :type_collaborator, :inclusion => { :in => %w{Asesor(a) Voluntario(a) Periodista},
                                                 :message => "%{value} no es un tipo de collaborador valido" }
