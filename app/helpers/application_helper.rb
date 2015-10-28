8d02476eb8b0fb7ec900dc77848488b044f3546b
mikesaurio <mikesaurio@gmail.com>
2015-10-28 10:54:22 -0600

    se modifica el CRUD de los colaboradores

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
-end
+
+  #regresa la liga de edit conforme al typo de usuario
+  def link_to_edit_profile(user)
+    case user.profile_type
+      when "Collaborator"
+       return edit_collaborator_es_path(Collaborator.where(email: user.email).last)
+      when "Investor"
+        return edit_investor_es_path(Investor.where(email: user.email).last)
+      when "Organization"
+       return edit_organization_es_path(Organization.where(email: user.email).last)
+      end 
+    end
+  end
