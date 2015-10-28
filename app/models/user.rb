ba4dac9cb1132678d5715e98413b92727535054f
Raúl Jiménez <jimenezjrs@gmail.com>
2015-10-28 13:12:57 -0600

    Fixed another issue where there was a tricky sintaxis error

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,15 +27,15 @@ class User < ActiveRecord::Base
   end
 
   def collaborator?
-    has_profile? 'Collaborator' && self.profile.present?
+    has_profile?('Collaborator') && self.profile.present?
   end
 
   def investor?
-    has_profile? 'Investor' && self.profile.present?
+    has_profile?('Investor') && self.profile.present?
   end
 
   def organization?
-    has_profile? 'Organization' && self.profile.present?
+    has_profile?('Organization') && self.profile.present?
   end
 
   def has_profile?(profile)
