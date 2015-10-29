c27d194fcc9c8c9a4153a289090e90412a8929ac
mikesaurio <mikesaurio@gmail.com>
2015-10-29 17:45:04 -0600

    se validan los casos de uso para las sumas de los imputs de fondeo de organizacion

--- app/assets/javascripts/organization.js
+++ app/assets/javascripts/organization.js
@@ -1,13 +1,10 @@
 function validate_private_public_sum(selected_button){
-    var private = $("#organization_rate_private option:selected").text();
-    var public = $("#organization_rate_public option:selected").text();
-    var auto = $("#organization_rate_auto option:selected").text(); 
 
     var private_id = $("#organization_rate_private").val();
     var public_id = $("#organization_rate_public").val();
     var auto_id = $("#organization_rate_auto").val(); 
 
-    if('private'){
+    if(selected_button == 'is_private'){
       var resta = parseInt(private_id) + parseInt(public_id);
       if(resta>100){
         document.getElementById('organization_rate_public').selectedIndex = 100-parseInt(private_id);
@@ -15,21 +12,21 @@ function validate_private_public_sum(selected_button){
       }else{
         document.getElementById('organization_rate_auto').selectedIndex = 100-resta;
       }
-    }else if('public'){
+    }else if(selected_button == 'is_public'){
       var resta = parseInt(private_id) + parseInt(public_id);
-      if(resta>100){
-        document.getElementById('organization_rate_private').selectedIndex = 100-parseInt(public_id);
+      if(resta > 100){
+        document.getElementById('organization_rate_private').selectedIndex = 100 - parseInt(public_id);
         document.getElementById('organization_rate_auto').selectedIndex = 0;
       }else{
         document.getElementById('organization_rate_auto').selectedIndex = 100-resta;
       }
-    }else if('auto'){
-      var resta = parseInt(private_id) + parseInt(public_id);
-      if(resta>100){
-        document.getElementById('organization_rate_public').selectedIndex = 100-parseInt(private_id);
-        document.getElementById('organization_rate_auto').selectedIndex = 0;
+    }else if(selected_button =='auto'){
+      var resta = parseInt(private_id) + parseInt(public_id) + parseInt(auto_id);
+      if(resta > 100){
+        document.getElementById('organization_rate_public').selectedIndex = 100- parseInt(auto_id);
+        document.getElementById('organization_rate_private').selectedIndex = 0;
       }else{
-        document.getElementById('organization_rate_auto').selectedIndex = 100-resta;
+        document.getElementById('organization_rate_private').selectedIndex = 100-resta;
       }
     }   
   }
