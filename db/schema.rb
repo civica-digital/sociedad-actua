ddf2a05b3ee86c25d8f5d75cad51c883b9b3a39a
Carlos Castellanos <rockarlos@me.com>
2015-10-30 17:08:34 -0600

    modification table for add new field on investor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030000703) do
+ActiveRecord::Schema.define(version: 20151030230025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(version: 20151030000703) do
 
   create_table "investors", force: :cascade do |t|
     t.string   "name"
-    t.text     "mantra"
-    t.text     "characteristics"
     t.string   "type_investor"
     t.string   "site_url"
     t.string   "blog_url"
@@ -52,12 +50,17 @@ ActiveRecord::Schema.define(version: 20151030000703) do
     t.string   "expense_type"
     t.string   "logo"
     t.string   "causes_supported"
+    t.string   "twitter_url"
+    t.string   "youtube_url"
+    t.string   "instagram_url"
+    t.string   "contact_name"
+    t.string   "legal_time"
   end
 
   create_table "organizations", force: :cascade do |t|
     t.string   "type_organization"
     t.string   "name"
-    t.datetime "remember_created_at", default: '2015-10-27 22:05:44', null: false
+    t.datetime "remember_created_at", default: '2015-10-20 06:18:53', null: false
     t.string   "email"
     t.string   "rfc"
     t.text     "mision"
@@ -73,6 +76,7 @@ ActiveRecord::Schema.define(version: 20151030000703) do
     t.string   "youtube"
     t.string   "instagram"
     t.integer  "foundation",          default: 2015
+    t.boolean  "show_street",         default: false
     t.boolean  "show_address",        default: false
     t.boolean  "show_colonia",        default: false
     t.boolean  "show_town",           default: false
