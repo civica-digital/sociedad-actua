b498cfa383b896cdcf29f8d272f6b972b9c79ffb
Carlos Castellanos <rockarlos@me.com>
2015-10-30 18:30:35 -0600

    url validations

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -15,6 +15,10 @@ class Investor < ActiveRecord::Base
  validates :site_url, format: { with: URI.regexp }, if: Proc.new { |a| a.site_url.present? }
  validates :facebook_url, format: { with: URI.regexp }, if: Proc.new { |a| a.facebook_url.present? }
  validates :blog_url, format: { with: URI.regexp }, if: Proc.new { |a| a.blog_url.present? }
+ validates :youtube_url, format: { with: URI.regexp }, if: Proc.new { |a| a.youtube_url.present? }
+ validates :instagram_url, format: { with: URI.regexp }, if: Proc.new { |a| a.instagram_url.present? }
+ validates :twitter_url, format: { with: URI.regexp }, if: Proc.new { |a| a.twitter_url.present? }
+
 
  validates :zipcode, :format => { :with => /[0-9]{5}/} ,presence:  true, on: :update
  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: [:create,:update] }, presence:  true, on: :update,if: Proc.new { |a| a.email.present? }
