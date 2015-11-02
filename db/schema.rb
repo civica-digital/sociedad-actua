a32442c92b3e2ef5e17977fe9db69d74402b754e
Carlos Castellanos <rockarlos@me.com>
2015-11-02 16:13:13 -0600

    add other causes to organization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102152157) do
+ActiveRecord::Schema.define(version: 20151102220454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20151102152157) do
     t.string "youtube_url"
     t.string "blog_url"
     t.string "logo"
+    t.string "other_causes"
     t.string "causes_interest"
   end
 
@@ -56,12 +57,13 @@ ActiveRecord::Schema.define(version: 20151102152157) do
     t.string   "instagram_url"
     t.string   "contact_name"
     t.string   "legal_time"
+    t.string   "other_causes"
   end
 
   create_table "organizations", force: :cascade do |t|
     t.string   "type_organization"
     t.string   "name"
-    t.datetime "remember_created_at", default: '2015-10-27 22:05:44', null: false
+    t.datetime "remember_created_at", default: '2015-10-20 06:18:53', null: false
     t.string   "email"
     t.string   "rfc"
     t.text     "mision"
@@ -77,6 +79,7 @@ ActiveRecord::Schema.define(version: 20151102152157) do
     t.string   "youtube"
     t.string   "instagram"
     t.integer  "foundation",          default: 2015
+    t.boolean  "show_street",         default: false
     t.boolean  "show_address",        default: false
     t.boolean  "show_colonia",        default: false
     t.boolean  "show_town",           default: false
@@ -91,6 +94,7 @@ ActiveRecord::Schema.define(version: 20151102152157) do
     t.integer  "rate_public",         default: 30
     t.integer  "rate_auto",           default: 40
     t.string   "profile_url"
+    t.string   "other_causes"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -113,6 +117,8 @@ ActiveRecord::Schema.define(version: 20151102152157) do
     t.float    "lat"
     t.float    "lng"
     t.string   "causes_interest"
+    t.string   "other_causes"
+    t.string   "clasification"
   end
 
   create_table "roles", force: :cascade do |t|
