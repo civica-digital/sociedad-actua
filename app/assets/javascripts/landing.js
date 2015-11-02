5ca49b840be97c4c14469384b664863b4475edf4
mikesaurio <mikesaurio@gmail.com>
2015-11-02 16:44:29 -0600

    se obtienen los datos electorales a partir de un geo

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -13,3 +13,23 @@ var markersArray = [];
         }  
       });
     }
+
+  function show_marker(){
+   var project_name = $("#project_name").val()+"";
+    if(project_name == null || project_name == "" ){
+      marker_visible(true,project_name)
+    }else{
+      marker_visible(false,project_name)
+    }
+
+    
+  }
+
+  function marker_visible(visible,project_name){
+    for (var i = 0; i < markersArray.length; i++ ) {
+      markersArray[i].serviceObject.setVisible(visible);
+      if(project_name-1 == i){
+        markersArray[i].serviceObject.setVisible(true);
+      }
+    }
+  }
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,6 @@
 class VisitorsController < ApplicationController
+before_action :set_values_map
+
   def tos
   end
 
@@ -9,15 +11,41 @@ class VisitorsController < ApplicationController
   end
 
   def index
-  	@projects = Project.all
+    
+  	@projects = Project.order(:name)
+    @projects.each_with_index do |project, index|
+      @array_lat[index] = project.lat
+      @array_lng[index] = project.lng
+      @array_name[index] = project.name
+    #get_electoral_information(@array_lat[index],@array_lng[index])
+      get_electoral_information("-100.30860900878906", "25.686706571891094")
+    end
+    
+  end
+
+
+  private
+  def set_values_map
     @projetc =  Project.new
     @array_lat=[]
     @array_lng=[]
     @array_name=[]
-  	@projects.each_with_index do |project, index|
-    	@array_lat[index] = project.lat
-    	@array_lng[index] = project.lng
-      @array_name[index] = project.name
-  	end
+    
+  end
+
+  def get_electoral_information(lat,lng)
+    require 'open-uri'
+    response = open("https://cicadmin.cartodb.com:443/api/v2/sql?q=select municipio, distrito, seccion from public.nl_secciones_electorales where ST_Within(ST_GeomFromText('POINT(#{lat} #{lng})', 4326), the_geom)").read
+    parsed = JSON.parse(response)
+    puts parsed["total_rows"]
+    if parsed["total_rows"].to_i > 0
+      parsed["rows"].each do |row|
+        puts row["municipio"]
+        puts row["distrito"]
+        puts row["seccion"]
+      end
+    end
+   
+
   end
 end
