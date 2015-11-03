13b7c0ecefed26df5773c0bb4c02a872ff00e65f
Carlos Castellanos <rockarlos@me.com>
2015-11-03 11:54:41 -0600

    añade la clasificacion ntee

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
 	belongs_to :organization
 	serialize :causes_interest, Array
+  serialize :clasification, Array
 	mount_uploader :photo_project, LogoUploader
 end
