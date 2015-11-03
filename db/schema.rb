1901edba27e3093e0d874b6d742249902aecfb5d
mikesaurio <mikesaurio@gmail.com>
2015-11-03 00:22:28 -0600

    se crea town en projects para mejorar busquedas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102115927) do
+ActiveRecord::Schema.define(version: 20151103061626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20151102115927) do
     t.string   "photo_project"
     t.float    "lat"
     t.float    "lng"
+    t.string   "town"
   end
 
   create_table "roles", force: :cascade do |t|
