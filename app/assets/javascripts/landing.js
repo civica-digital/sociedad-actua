bd7a17c7b4c72d8bb1355564c4ebbba83e87d49b
mikesaurio <mikesaurio@gmail.com>
2015-11-03 00:00:58 -0600

    el mapa se centra al buscar un marcador

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -28,8 +28,11 @@ var markersArray = [];
   function marker_visible(visible,project_name){
     for (var i = 0; i < markersArray.length; i++ ) {
       markersArray[i].serviceObject.setVisible(visible);
+      handler.getMap().setZoom(12);
       if(project_name-1 == i){
         markersArray[i].serviceObject.setVisible(true);
+        handler.map.centerOn(markersArray[i].serviceObject.position)
+        handler.getMap().setZoom(18);
       }
     }
   }
