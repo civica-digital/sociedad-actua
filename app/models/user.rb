117cd11f0702dc6aac22f80b7067ecc474d21a7c
Raúl Jiménez <jimenezjrs@gmail.com>
2015-11-04 07:53:11 -0600

    removed create_resource code duplication

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,9 +32,7 @@ class User < ActiveRecord::Base
   end
 
   def collaborator?
-  
     has_profile?('Collaborator') && self.profile.present?
-    
   end
 
   def investor?
@@ -43,22 +41,10 @@ class User < ActiveRecord::Base
 
   def organization?
     has_profile?('Organization') && self.profile.present?
-
   end
 
   def has_profile?(profile)
     self.profile_type == profile
-
-  end
-
-  
-
-  def create_resource 
-    
-      @organization=Organization.create
-      
-
-        
   end
 
   private
