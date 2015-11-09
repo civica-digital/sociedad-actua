1909b5c65f61fff2a2342abdb34fe4e1bfef7e37
Carlos Castellanos <rockarlos@me.com>
2015-11-09 16:45:23 -0600

    generate events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,14 @@
 class Event < ActiveRecord::Base
 	validates :name, :description, :date,:time, :address, :notes, presence:  true
-    belongs_to :organization
-    before_save :validate_causes
-    serialize :causes, Array
-    mount_uploader :image, LogoUploader
+  belongs_to :organization
+  before_save :validate_causes
+  serialize :causes, Array
+  mount_uploader :image, LogoUploader
 
-    private
-	  def validate_causes
-	    if (self.causes[0]== "")
-	      self.causes.delete("")
-	    end  
-	  end
+  private
+  def validate_causes
+    if (self.causes[0]== "")
+      self.causes.delete("")
+    end  
+  end
 end
