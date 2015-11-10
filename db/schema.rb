fff8f993de5ad3d36cfe769b6f5a7b83e2e57ab5
Carlos Castellanos <rockarlos@me.com>
2015-11-10 11:26:24 -0600

    migracion para cambio de datos y orden en el form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109231512) do
+ActiveRecord::Schema.define(version: 20151110170737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20151109231512) do
     t.text     "description"
     t.datetime "date"
     t.text     "notes"
-    t.decimal  "price"
+    t.integer  "price"
     t.string   "image"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
