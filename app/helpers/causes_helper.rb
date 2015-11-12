b58a4de85aff04a917e616341dfdf51ac63f1274
carlos <rockarlos@me.com>
2015-11-12 12:47:37 -0600

    cambio de variable

--- app/helpers/causes_helper.rb
+++ app/helpers/causes_helper.rb
@@ -1,10 +1,10 @@
 module CausesHelper
 	def fetch_causes_from(projects)
-		@causes=[]
+		causes=[]
 		projects.each do |project|
-		  @causes+= project.causes_interest
+		  causes+= project.causes_interest
 		end
-		return sort_causes(@causes)
+		return sort_causes(causes)
 	end
 	def sort_causes(causes)
 		return causes.sort_by{|cause| cause.downcase}.uniq
