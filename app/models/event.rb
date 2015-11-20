171680f8c81c27d0ebc8d3cd653d4ab4c28d8b1a
carlos <rockarlos@me.com>
2015-11-20 15:30:26 -0600

    validacion de campos y traducciones

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
+	validates :name, :description, :date,:time, :address, :notes, presence:  true
     belongs_to :organization
     serialize :causes, Array
     mount_uploader :image, LogoUploader
