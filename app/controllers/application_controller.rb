3f318e8b904ec65bdbe1c9aa3881f67781ec8d11
Juan Pablo Escobar <juannpablo@gmail.com>
2016-02-03 20:02:02 -0600

    Adds Pundit rescue for not authorized users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   after_action :verify_authorized, except: :index, unless: :devise_controller?
 
-  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
-  # def user_not_authorized
-  #   flash[:alert] = "You are not authorized to perform this action."
-  #   redirect_to(request.referrer || root_path)
-  # end
+  def user_not_authorized
+    flash[:alert] = I18n.t('user_not_authorized')
+    redirect_to(request.referrer || root_path)
+  end
 end
